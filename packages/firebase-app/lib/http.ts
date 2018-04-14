081ac0732773c16beb333c03714580cda30fa55b
Patrick Hulce <patrick.hulce@gmail.com>
2018-04-14 13:45:50 -0700

    fix(firebase-app): remove unused require

--- packages/firebase-app/lib/http.ts
+++ packages/firebase-app/lib/http.ts
@@ -1,5 +1,4 @@
 import * as functions from 'firebase-functions'
-import './conf' // tslint:disable-line
 import {app} from '../../api/lib/app'
 
 export const api = functions.https.onRequest(app)
--- packages/firebase-app/scripts/deploy-to-prod.js
+++ packages/firebase-app/scripts/deploy-to-prod.js
@@ -3,6 +3,7 @@
 const path = require('path')
 const shell = require('shelljs')
 
+shell.rm('-rf', path.join(__dirname, '../dist-firebase'))
 shell.exec('NODE_ENV=firebase-production npm run build')
 process.chdir(path.join(__dirname, '../dist-firebase'))
 shell.exec('firebase deploy')
