a235c714dd51c82f7f8e73ed9c1b2c2b0ccf7641
Patrick Hulce <patrick.hulce@gmail.com>
2018-05-02 22:24:01 -0700

    chore(scripts): skip build on database sync

--- scripts/sync-database.js
+++ scripts/sync-database.js
@@ -3,13 +3,15 @@
 const fs = require('fs')
 const shelljs = require('shelljs')
 
+const FIREBASE_DIST = 'packages/firebase-app/dist-firebase/dist'
+
 async function run() {
-  if (!fs.existsSync('../packages/firebase-app/dist-firebase/dist/api/lib/')) shelljs.exec('npm run build')
+  if (!fs.existsSync(`${FIREBASE_DIST}/api/lib/`)) shelljs.exec('npm run build')
 
   // Load the API to register all necessary models
-  require('../packages/firebase-app/dist-firebase/dist/api/lib/app')
+  require(`../${FIREBASE_DIST}/api/lib/app`)
   // Load in the SQL extension to sync
-  const sqlExtension = require('../packages/firebase-app/dist-firebase/dist/shared/lib').sqlExtension
+  const sqlExtension = require(`../${FIREBASE_DIST}/shared/lib`).sqlExtension
 
   console.log('Syncing database...')
   await sqlExtension.sequelize.sync({force: true})
