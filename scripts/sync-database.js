1ac425ba8467bfe8758523b3e53710e8cc61587d
Patrick Hulce <patrick.hulce@gmail.com>
2018-05-03 22:44:52 -0700

    chore(scripts): add migration update script

--- scripts/sync-database.js
+++ scripts/sync-database.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
+const path = require('path')
 const shelljs = require('shelljs')
 
-const FIREBASE_DIST = 'packages/firebase-app/dist-firebase/dist'
+const FIREBASE_DIST = path.join(__dirname, '../packages/firebase-app/dist-firebase/dist')
 
 async function run() {
   if (!fs.existsSync(`${FIREBASE_DIST}/api/lib/`)) shelljs.exec('npm run build')
 
   // Load the API to register all necessary models
-  require(`../${FIREBASE_DIST}/api/lib/app`)
+  require(`${FIREBASE_DIST}/api/lib/app`)
   // Load in the SQL extension to sync
-  const sqlExtension = require(`../${FIREBASE_DIST}/shared/lib`).sqlExtension
+  const sqlExtension = require(`${FIREBASE_DIST}/shared/lib`).sqlExtension
 
   console.log('Syncing database...')
   await sqlExtension.sequelize.sync({force: true})
---
+++ scripts/update-migration.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const _ = require('lodash')
+const fs = require('fs')
+const path = require('path')
+const shelljs = require('shelljs')
+
+const KLAY_EXECUTABLE = path.join(__dirname, '../node_modules/.bin/klay-kiln-sql')
+const LINT_EXECTUABLE = path.join(__dirname, '../node_modules/.bin/lint')
+
+const MIGRATION_FOLDER = path.join(__dirname, '../migrations')
+const DIST_FOLDER = path.join(__dirname, '../packages/firebase-app/dist-firebase/dist')
+const KILN_FILE = path.join(DIST_FOLDER, 'shared/lib/kiln.js')
+
+async function run() {
+  const filesBefore = fs.readdirSync(MIGRATION_FOLDER)
+  shelljs.exec(`${KLAY_EXECUTABLE} migration:bootstrap -k ${KILN_FILE}`)
+  const filesAfter = fs.readdirSync(MIGRATION_FOLDER)
+
+  const bootstrapFile = path.join(MIGRATION_FOLDER, filesBefore.find(f => /bootstrap.js$/.test(f)))
+  const createdFile = path.join(MIGRATION_FOLDER, _.difference(filesAfter, filesBefore)[0])
+  shelljs.exec(`${LINT_EXECTUABLE} --fix --ignore '*.ts' --prettify ${createdFile}`, {silent: true})
+  shelljs.mv(createdFile, bootstrapFile)
+}
+
+
+run().catch(err => {
+  console.error(err)
+  process.exit(1)
+})
