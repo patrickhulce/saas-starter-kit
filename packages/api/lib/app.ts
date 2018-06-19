ee10e207fdc1f891831c7aa3bdc003f5a236056c
Patrick Hulce <patrick.hulce@gmail.com>
2018-06-18 22:12:19 -0700

    chore: bump klay dependency

--- packages/api/lib/app.ts
+++ packages/api/lib/app.ts
@@ -1,15 +1,18 @@
 import {json} from 'body-parser'
 import * as cookies from 'cookie-parser'
 import * as express from 'express'
-import {createAndMergeRouters, createGrantCreationMiddleware} from 'klay'
+import {
+  createAndMergeRouters,
+  createGrantCreationMiddleware,
+  createHandleErrorMiddleware,
+  createHandlePromiseMiddleware,
+} from 'klay'
 import {omit, pick} from 'lodash'
 
 import {authConfiguration, kiln, sqlExtension} from '../../shared/lib'
 
 import {setPrivateCacheControl} from './middleware/cache-control'
-import {handleError} from './middleware/handle-error'
 import {handlePageRequest, pageRoutes} from './middleware/handle-page-request'
-import {handlePromise} from './middleware/handle-promise'
 import {loggers} from './middleware/logging'
 import {routerMap} from './routes'
 import './typedefs' // tslint:disable-line
@@ -25,7 +28,7 @@ app.use(createGrantCreationMiddleware(authConfiguration))
 app.get(pageRoutes, handlePageRequest)
 app.use('/api/v1', createAndMergeRouters(kiln, routerMap).router)
 
-app.use(handlePromise)
-app.use(handleError)
+app.use(createHandlePromiseMiddleware())
+app.use(createHandleErrorMiddleware())
 
 export {app, sqlExtension}
